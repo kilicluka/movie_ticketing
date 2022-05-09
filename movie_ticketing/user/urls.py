d75ee672c528049259e65e626e8e764b20afece6
Luka Kilic <kilic.luka@gmail.com>
2022-05-09 10:37:46 +0200

    add jwt auth

--- movie_ticketing/user/urls.py
+++ movie_ticketing/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .views import UserProfileCreateView
 
 urlpatterns = [
     path("create/", UserProfileCreateView.as_view(), name="user_create"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
