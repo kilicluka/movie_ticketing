ce236e6b26857c998c48724e42562b9ae4e52ba7
Luka Kilic <kilic.luka@gmail.com>
2022-05-11 10:14:53 +0200

    add ability for user to fetch their reservations

--- movie_ticketing/reservations/views.py
+++ movie_ticketing/reservations/views.py
@@ -1,12 +1,18 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
+from .models import Reservation
 from .serializers import ReservationsSerializer
 
 
 class ReservationsView(generics.ListCreateAPIView):
     serializer_class = ReservationsSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Reservation.objects.filter(user=self.request.user).prefetch_related(
+            "seats"
+        )
 
     def post(self, request, *args, **kwargs):
         request.data.update({"user": request.user.pk})
